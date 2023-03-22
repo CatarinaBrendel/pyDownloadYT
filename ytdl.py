e08cb5dee64600e4286f4a1eba3780291adf71e5
katebrend <42495865+CatarinaBrendel@users.noreply.github.com>
2023-03-22 10:20:14 +0100

    improved error handling

--- ytdl.py
+++ ytdl.py
@@ -4,15 +4,18 @@ import os
 
 yt = YouTube(str(input("Enter the URL of the video to be downloaded: \n>> ")), on_progress_callback=on_progress)
 
-video = yt.streams.filter(only_audio=True).first();
+try:
+    video = yt.streams.filter(only_audio=True).first()
+except:
+    print(f'Video {yt} is unavailable, nothing will be downloaded')
+else:
+    print("Enter destination folder")
+    folder = str(input(">> ")) or '.'
 
-print("Enter destination folder")
-folder = str(input(">> ")) or '.'
+    out_file = video.download(output_path=folder)
 
-out_file = video.download(output_path=folder)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
 
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
-
-print(yt.title + " downloaded");
+    print(yt.title + " downloaded");
